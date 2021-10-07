ac47c68c2dd405cbdbfb4206152ee210e5636bff
sonhosu <ghtn9398@nate.com>
2021-10-07 16:16:21 +0900

    간단한 텍스트화면 만들기

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const cat_name ='perl';
-  const styles = {color: "orange"};
+
+
+  const styles = {
+    color: "green",
+    border: '1px solid #eee',
+    padding: '20px',
+    display: 'flex', //
+    width:'100vw',
+    maxWidth:'400px',
+    flexDirection:'column',//세로정렬
+
+  };
+
 
   return (
     <div className="App">
-     <p style={styles}>orange</p>
+      <div style={styles}>
+        <h1 style={{color: 'green'}}>안녕하세요!</h1>
+        <hr style={{with: '100%'} }/>
+        <p style={{textAlign: 'left'}}>이름을 입력해주세요.</p>
+        <input type="text"></input>
+      </div>
     </div>
   );
 }
