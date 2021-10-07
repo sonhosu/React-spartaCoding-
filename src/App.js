e823cb99ab9da6c784c071c9ff8e4ad9bae56a7c
sonhosu <ghtn9398@nate.com>
2021-10-07 16:34:32 +0900

    함수형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// BucketList 컴포넌트를 import 해옵니다.
+// import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
+import BucketList from './BucketList';
 
 function App() {
 
-
-  const styles = {
-    color: "green",
-    border: '1px solid #eee',
-    padding: '20px',
-    display: 'flex', //
-    width:'100vw',
-    maxWidth:'400px',
-    flexDirection:'column',//세로정렬
-
-  };
-
-
   return (
     <div className="App">
-      <div style={styles}>
-        <h1 style={{color: 'green'}}>안녕하세요!</h1>
-        <hr style={{with: '100%'} }/>
-        <p style={{textAlign: 'left'}}>이름을 입력해주세요.</p>
-        <input type="text"></input>
-      </div>
+      <h1>내 버킷리스트</h1>
+      {/* 컴포넌트를 넣어줍니다. */}
+      <BucketList/>
     </div>
   );
 }
